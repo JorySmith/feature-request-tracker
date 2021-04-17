c5fdec2519631764f18460f1382e1a9563b2b06c
Jory <46983170+Cambro320@users.noreply.github.com>
2021-04-17 16:09:54 -0500

    adding Menu component

---
+++ src/components/Menu.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react";
+
+const Menu = () => {
+  return (
+    <Fragment>
+
+      <div className="menu-container">
+        <h5>Duncan Aviation</h5>
+        <p><a href=".">Dashboard</a></p>
+      </div>
+
+    </Fragment>
+  )
+}
+
+export default Menu
